60b5798c84b20c8182b737ec8527c6ec0cfc9448
entbappy <entbappy73@gmail.com>
2026-03-07 22:44:43 +0600

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mosquito-detector",
+    version="0.1.0",
+    author="Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
